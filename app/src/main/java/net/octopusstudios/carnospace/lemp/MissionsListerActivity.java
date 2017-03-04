f8fa2688721e2a178545fd024329a44707ba3dc1
Need2Revolt <francesco.davide.carnovale@gmail.com>
2017-03-04 19:45:41 +0100

    Adding mission list using a ListView, using theme colors

--- app/src/main/java/net/octopusstudios/carnospace/lemp/MissionsListerActivity.java
+++ app/src/main/java/net/octopusstudios/carnospace/lemp/MissionsListerActivity.java
@@ -1,29 +1,58 @@
 package net.octopusstudios.carnospace.lemp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import net.octopusstudios.carnospace.lemp.adapters.MissionsAdapter;
+import net.octopusstudios.carnospace.lemp.pojo.Mission;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MissionsListerActivity extends AppCompatActivity {
 
+    private MissionsAdapter missionsAdapter;
+
+    private List<Mission> missions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //setup main UI
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_missions_lister);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        //setup internal objects
+        missions = new ArrayList<Mission>(0);
+
+        ListView missionsList = (ListView) findViewById(R.id.missionsListView);
+        missionsAdapter = new MissionsAdapter(this, missions);
+        missionsList.setAdapter(missionsAdapter);
+
+        final Context ctx = this;
+        //TODO restore this
+        //fab.setOnClickListener(new AddMissionListener(ctx, missions, missionsAdapter));
+
+        //experimental
+        missionsList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                //TODO restore this
+                //Intent intent = new Intent(ctx, MissionDetails.class);
+                //startActivity(intent);
             }
         });
     }
@@ -49,4 +78,15 @@ public class MissionsListerActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
 }
