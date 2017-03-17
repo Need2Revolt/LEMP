cdd1262ee76765cf16fb40aa0949eb6a55c14be8
Need2Revolt <francesco.davide.carnovale@gmail.com>
2017-03-17 23:43:45 +0100

    Mission deletion is here
    persistence overall looks solid atm... time for a new alpha...

--- app/src/main/java/net/octopusstudios/carnospace/cmp/activity/MissionsListerActivity.java
+++ app/src/main/java/net/octopusstudios/carnospace/cmp/activity/MissionsListerActivity.java
@@ -12,6 +12,8 @@ import android.support.design.widget.FloatingActionButton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,7 +23,9 @@ import android.widget.ListView;
 import net.octopusstudios.carnospace.cmp.R;
 import net.octopusstudios.carnospace.cmp.adapter.MissionsAdapter;
 import net.octopusstudios.carnospace.cmp.listener.AddMissionListener;
+import net.octopusstudios.carnospace.cmp.pojo.DaoSession;
 import net.octopusstudios.carnospace.cmp.pojo.Mission;
+import net.octopusstudios.carnospace.cmp.pojo.Stage;
 import net.octopusstudios.carnospace.cmp.status.SharedState;
 
 import java.util.List;
@@ -32,7 +36,7 @@ import java.util.List;
 public class MissionsListerActivity extends AppCompatActivity {
 
     private MissionsAdapter missionsAdapter;
-
+    private DaoSession daoSession;
     private List<Mission> missions;
 
     @Override
@@ -47,15 +51,18 @@ public class MissionsListerActivity extends AppCompatActivity {
         //setup internal objects
         final SharedState sharedState = (SharedState) getApplicationContext();
         missions = sharedState.getMissions();
+        daoSession = sharedState.getDaoSession();
 
-        ListView missionsList = (ListView) findViewById(R.id.missionsListView);
+        ListView missionsListView = (ListView) findViewById(R.id.missionsListView);
         missionsAdapter = new MissionsAdapter(this, missions);
-        missionsList.setAdapter(missionsAdapter);
+        missionsListView.setAdapter(missionsAdapter);
+
+        registerForContextMenu(missionsListView);
 
         final Context ctx = this;
         fab.setOnClickListener(new AddMissionListener(ctx, missions, missionsAdapter));
 
-        missionsList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        missionsListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -66,6 +73,34 @@ public class MissionsListerActivity extends AppCompatActivity {
         });
 
         missionsAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        if (v.getId()==R.id.missionsListView) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.missions_list_menu, menu);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch(item.getItemId()) {
+
+            case R.id.deleteMission:
+                Mission toDelete = missions.remove(info.position);
+                for(Stage s : toDelete.getMissionStages()) {
+                    daoSession.delete(s);
+                }
+                daoSession.delete(toDelete);
+                missionsAdapter.notifyDataSetChanged();
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 
     @Override
